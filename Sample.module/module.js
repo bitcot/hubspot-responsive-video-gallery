6166c899249ea6bfd324fa493a3fd91846480707
bharat bhokre <bharat@bitcot.com>
2020-01-30 18:49:00 +0530

    First master commit

---
+++ Sample.module/module.js
@@ -0,0 +1,57 @@
+jQuery(document).ready(function() {    	
+  setTimeout(function() {
+    jQuery('.popup-gallery').magnificPopup({
+    		delegate: 'a',
+    		type: 'image',
+        callbacks: {
+          elementParse: function(item) {
+            // Function will fire for each target element
+            // "item.el" is a target DOM element (if present)
+            // "item.src" is a source that you may modify
+            console.log(item.el.context.classList[0]);
+            if(item.el.context.classList[0] == 'video') {
+              item.type = 'iframe',
+              item.iframe = {
+                 patterns: {
+                   youtube: {
+                     index: 'youtube.com/', // String that detects type of video (in this case YouTube). Simply via url.indexOf(index).
+
+                     id: 'v=', // String that splits URL in a two parts, second part should be %id%
+                      // Or null - full URL will be returned
+                      // Or a function that should return %id%, for example:
+                      // id: function(url) { return 'parsed id'; } 
+
+                     src: '//www.youtube.com/embed/%id%?autoplay=1' // URL that will be set as a source for iframe. 
+                   },
+                   vimeo: {
+                     index: 'vimeo.com/',
+                     id: '/',
+                     src: '//player.vimeo.com/video/%id%?autoplay=1'
+                   },
+                   gmaps: {
+                     index: '//maps.google.',
+                     src: '%id%&output=embed'
+                   }
+                 }
+              }
+            } else {
+               item.type = 'image',
+               item.tLoading = 'Loading image #%curr%...',
+               item.mainClass = 'mfp-img-mobile',
+               item.image = {
+                 tError: '<a href="%url%">The image #%curr%</a> could not be loaded.'
+               }
+            }
+
+          }
+        },
+    		gallery: {
+    			enabled: true,
+    			navigateByImgClick: true,
+    			preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+    		},
+       mainClass: 'mfp-fade mfp-video',
+        
+    	});
+  }, 1200 );
+});
---
+++ readme.md
@@ -0,0 +1,29 @@
+![HubSpot](https://cdn2.hubspot.net/hubfs/327485/HubSpot%20Wordmark%20-%20Full%20Color.png "HubSpot")
+## Responsive Video Gallary
+
+---
+
+# Description:
+Video gallery really enhances the rating of your site when people search for related topics and videos whether on Youtube and Vimeo or search engines in general. Getting your Youtube, Vimeo  extension working is straightforward with a few simple steps required to build these great video resources on your Hubspot site.
+       The process of creating a video gallery only takes a few minutes and created gallery can be displayed on any page or post of HubSpot shortcode and no matter how small your device screens are it will automatically adjust to fit on that screens and display perfectly. With the drag and drop image position feature, you can arrange the order of the image that has to be displayed in your gallery.
+
+# Template Usage code:
+{% module "module_158037018986363" path="/Video gallery with popup", label="Video gallery with popup" %}
+
+# Features:
+- Responsive and touch-friendly.
+- CSS powered animations.
+- Keyboard / Touch swipe navigation.
+- Custom image aspect ratio.
+- Set Column Layouts
+- LightBox Pop-up Style
+- video Order Setting Like Ascending, Descending & Shuffle
+- Easy To Implement Anywhere Into Hubspot
+- Easily Customization
+- Multi-Language supporting (English, English-UK, English-Australia, English-US, Greek, German, Danish-denmark)
+
+# Demo 
+- http://dev-dev-7029090-7029090.hs-sites.com/video-gallery
+
+# Support:
+If you find any issue or need help, please ask us. We are active in https://help.hubspot.com/ Support Forum.
